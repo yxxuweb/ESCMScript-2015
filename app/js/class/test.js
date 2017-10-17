647ee009d6be219700994b91443680c41f1a38f0
yxxu <yxxu_web@sina.com>
2017-10-17 22:11:50 +0800

    es6 学习

---
+++ app/js/class/test.js
@@ -0,0 +1,10 @@
+class Test{
+    constructor(){
+        this.a = 'hello world123'
+    }
+}
+
+
+let test = new Test();
+
+document.body.innerHTML = test.a;
---
+++ app/js/index.js
@@ -0,0 +1,20 @@
+import 'babel-polyfill';
+
+import Lottery from './lottery'
+
+const syy = new Lottery();
+console.log(syy);
+/**
+ * 性能优化
+ * 1. 多使用内存，缓存
+ * 2. 减少网络请求
+ * 3. 静态资源压缩合并，静态资源缓存
+ * 4. 使用cdn让资源加载更快
+ * 5. 使用SSR后端渲染
+ * 6. css放前面，js放后面
+ * 7. 懒加载
+ * 8. 减少DOM查询，对DOM查询做缓存
+ * 9. 减少DOM操作，多个操作合并在一起
+ * 10. 事件节流
+ * 11. 尽早执行操作
+ */
