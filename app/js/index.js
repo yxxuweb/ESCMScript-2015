1f725a948a9cc1485704c34720362ee2fa5fc7d8
yxxu <yxxu_web@sina.com>
2017-11-09 21:17:26 +0800

    修改

--- app/js/index.js
+++ app/js/index.js
@@ -9,12 +9,3 @@ var obj = {
 
 console.log(obj[Symbol.for('foo')]);
 console.log(obj[a]);
-
-var arr = [1,2,3,4,5,1,2,3,4,5];
-
-var s = new Set(arr);
-
-for (let key of s) {
-    console.log(key);
-}
-s.map((value, key) => console.log(key + ' : ' + value))
